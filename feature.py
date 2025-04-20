cc7900d9a86baac3cd59ee97f33df3455d838d94
Marcus Loh <marcusloh1212@gmail.com>
2025-04-20 16:42:37 +0800

    add feature.py on main branch to simulate merge conflict with featurex

---
+++ feature.py
@@ -0,0 +1,9 @@
+# Prompt user for input
+x = float(input("Enter the first number (x): "))
+y = float(input("Enter the second number (y): "))
+
+# Calculate the sum with a small constant added
+z = x + y + 0.12345
+
+# Display the result
+print("The sum of x and y is:", z)
