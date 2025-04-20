52525fb273ddb0b96fed1f59c560354760d7caf6
Marcus Loh <marcusloh1212@gmail.com>
2025-04-20 16:56:55 +0800

    Add feature 2 in collaborator to simulate collaborator extra file

---
+++ feature_2.py
@@ -0,0 +1,9 @@
+# Prompt user for input
+x = float(input("Enter the first number (x): "))
+y = float(input("Enter the second number (y): "))
+
+# Calculate the product
+z = x * y
+
+# Display the result
+print("The product of x and y is:", z)
